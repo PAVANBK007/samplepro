94af9e52e9c1429f04d7b0bee5f2660d9ab5a316
PAVANBK007 <38972802+PAVANBK007@users.noreply.github.com>
2021-06-30 22:27:40 +0530

    Update ServletServer.java

--- src/main/java/org/openshift/quikstarts/undertow/servlet/ServletServer.java
+++ src/main/java/org/openshift/quikstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "H pavan")
+                                    .addInitParam("message", "Hello b pavan")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
